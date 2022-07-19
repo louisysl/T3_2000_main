4a2bb4287830a6aa73bf57bea724c38ea733cec4
louisysl <louis.loehr@t-online.de>
2022-07-19 12:15:16 +0200

    repair

--- main/convert.bat
+++ main/convert.bat
@@ -1,6 +1,6 @@
 :: PANDOC.exe PATH LAPTOP: "C:\Users\a798583\AppData\Local\Programs\Python\Python310\Scripts\pandoc-fignos.exe"
-pandoc "C:\Users\a798583\OneDrive - Atos\Documents\GitHub\T3_2000_main\main\PA2022.md" -f markdown -t latex --template "C:\Users\a798583\OneDrive - Atos\Documents\GitHub\T3_2000_main\main\tex\template.tex" --filter "C:\Users\a798583\AppData\Local\Programs\Python\Python310\Scripts\pandoc-fignos.exe" --toc --toc-depth=6 -N --citeproc --bibliography "C:\Users\a798583\OneDrive - Atos\Documents\GitHub\T3_2000_main\main\t3_2000.bib" --csl "C:\Users\a798583\OneDrive - Atos\Documents\GitHub\T3_2000_main\main\tex\thieme-german.csl" --pdf-engine=xelatex --mathjax 
+pandoc "C:\Users\a798583\OneDrive - Atos\Documents\GitHub\T3_2000_main\main\PA2022.md" -f markdown -t latex --template "C:\Users\a798583\OneDrive - Atos\Documents\GitHub\T3_2000_main\main\tex\template.tex" --filter "C:\Users\a798583\AppData\Local\Programs\Python\Python310\Scripts\pandoc-fignos.exe" --toc --toc-depth=6 -N --citeproc --bibliography "C:\Users\a798583\OneDrive - Atos\Documents\GitHub\T3_2000_main\main\t3_2000.bib" --csl "C:\Users\a798583\OneDrive - Atos\Documents\GitHub\T3_2000_main\main\tex\thieme-german.csl" --pdf-engine=xelatex --mathjax -o "Projektarbeit.pdf"
 if %errorlevel% neq 0 exit /b %errorlevel% 
-"Projektarbeit.pdf"
+code "Projektarbeit.pdf"
 
 :: pdf open (edit in pandoc line) -o "Projektarbeit.pdf"
